886110e8af19025454b596404585b322bf10f0c8
ShahinHossaain <mdshahinhossaain@gmail.com>
2025-04-22 15:20:53 +0600

    change

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING)
 // REQUEST PARSERS 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('./stylesheets/style.css'));
 
 
